033838c01209b4dac2eaac3091101ea9d4827b6b
kumarbha <bhavana.kumar@diconium.com>
2023-02-07 14:47:45 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Bhavana Kumar on 07.02.23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
