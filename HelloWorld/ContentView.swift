bd28c2ffa57529fdb2955acc4a379e05dc19d676
kumarbha <bhavana.kumar@diconium.com>
2023-02-07 16:10:09 +0100

    test changes

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world ios test!")
+            Text("Hello, world :)!")
         }
         .padding()
     }
